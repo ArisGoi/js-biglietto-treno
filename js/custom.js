e6c19530b15a8025a993b78be9e42eab5b280802
ArisGoi <aris.goi97@gmail.com>
2021-07-29 16:23:00 +0200

    add nuovo metodo di input + bottone per trigger funzione calcolo

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,29 @@
-// 1) Ottengo i dati dall'utente
-var distanceKm = parseInt(prompt('Quanti Km vuoi percorrere?'));
-var userEta = parseInt(prompt('Quanti anni hai?'));
+// 0) attivo la funzione al click del bottone
+document.getElementById("calculate").addEventListener("click", calcoloPrezzo);
 
-// 2) Calcolo il prezzo in base ai Km
-var ticketPrice = distanceKm * 0.21; //Prezzo: 0.21€ al Km
+function calcoloPrezzo() {
 
-// 3) divido gli sconti per età e ottengo il prezzo finale
-var sconto;
+    // 1) Ottengo i dati dall'utente
+    var distanceKm = parseInt(document.getElementById('input_Km').value);
+    var userEta = parseInt(document.getElementById('input_eta').value);
 
-if (userEta < 18){
-    sconto = (ticketPrice * 20 /100);
-} else if (userEta >= 65){
-    sconto = (ticketPrice * 40 /100);
-} else {
-    sconto = 0;
+    // 2) Calcolo il prezzo in base ai Km
+    var ticketPrice = distanceKm * 0.21; //Prezzo: 0.21€ al Km
+
+    // 3) divido gli sconti per età e ottengo il prezzo finale
+    var sconto;
+
+    if (userEta < 18){
+        sconto = (ticketPrice * 20 /100);
+    } else if (userEta >= 65){
+        sconto = (ticketPrice * 40 /100);
+    } else {
+        sconto = 0;
+    }
+
+    var finalPrice = ticketPrice - sconto;
+    finalPrice = (Math.round(finalPrice * 100) / 100).toFixed(2);
+
+    // output = Prezzo finale del biglietto
+    alert('Il prezzo del biglietto è: ' + finalPrice + ' €');
 }
-
-var finalPrice = ticketPrice - sconto;
-finalPrice = (Math.round(finalPrice * 100) / 100).toFixed(2);
-
-// output = Prezzo finale del biglietto
-alert('Il prezzo del biglietto è: ' + finalPrice + ' €');
