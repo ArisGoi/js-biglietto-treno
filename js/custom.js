dc8c164649564039464f62a112ca4fb1a183b372
ArisGoi <aris.goi97@gmail.com>
2021-07-29 15:49:30 +0200

    add - calcolo prezzo

--- js/custom.js
+++ js/custom.js
@@ -13,3 +13,18 @@ var userEta = parseInt(prompt('Quanti anni hai?'));
 // 2) Calcolo il prezzo in base ai Km
 var ticketPrice = distanceKm * 0.21; //Prezzo: 0.21€ al Km
 
+// 3) divido gli sconti per età e ottengo il prezzo finale
+var sconto;
+
+if (userEta < 18){
+    sconto = (ticketPrice * 20 /100);
+} else if (userEta > 65){
+    sconto = (ticketPrice * 40 /100);
+} else {
+    sconto = 0;
+}
+
+var finalPrice = ticketPrice - sconto;
+
+// output = Prezzo finale del biglietto
+alert('Il prezzo del biglietto è: ' + finalPrice + ' €');
