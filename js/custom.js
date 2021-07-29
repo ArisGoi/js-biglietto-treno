a44fd7466b890c842155dde03a546ebdd8dca6f1
ArisGoi <aris.goi97@gmail.com>
2021-07-29 15:56:30 +0200

    add (rimozioni decimali) + migliorati criteri età

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,3 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km)
-// va applicato uno sconto del 20% per i minorenni
-// va applicato uno sconto del 40% per gli over 65.
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
-
-
 // 1) Ottengo i dati dall'utente
 var distanceKm = parseInt(prompt('Quanti Km vuoi percorrere?'));
 var userEta = parseInt(prompt('Quanti anni hai?'));
@@ -18,13 +10,14 @@ var sconto;
 
 if (userEta < 18){
     sconto = (ticketPrice * 20 /100);
-} else if (userEta > 65){
+} else if (userEta >= 65){
     sconto = (ticketPrice * 40 /100);
 } else {
     sconto = 0;
 }
 
 var finalPrice = ticketPrice - sconto;
+finalPrice = (Math.round(finalPrice * 100) / 100).toFixed(2);
 
 // output = Prezzo finale del biglietto
 alert('Il prezzo del biglietto è: ' + finalPrice + ' €');
